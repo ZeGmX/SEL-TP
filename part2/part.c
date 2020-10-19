5f80c085c5fc25aa55386024bb0862e0641cbfbe
ZeGmX <lendy.mulot@gmail.com>
2020-10-19 10:45:56 +0200

    Continued challenge 2

--- part2/part.c
+++ part2/part.c
@@ -4,12 +4,13 @@
 #include <libelf.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 #define SIZE 1024
 
 // Finds and returns the PID of the target process
 pid_t find_process(char* ownername, char* process) {
     char command[SIZE];
-    
+
     sprintf(command, "pgrep -u %s %s\n", ownername, process);
     FILE* pgrep = popen(command, "r");
 
@@ -17,10 +18,10 @@ pid_t find_process(char* ownername, char* process) {
         printf("Error : Failed to run pgrep with popen().\n");
         return 0;
     }
-    
+
     int pid = 0;
     fscanf(pgrep, "%d", &pid);
-    
+
     if (pclose(pgrep) != 0) {
         printf("Error : pgrep returned a non-zero value.\nAre you sure this process exists ?\n");
         return 0;
@@ -32,7 +33,7 @@ pid_t find_process(char* ownername, char* process) {
 int get_function_offset(char* target_process, char* target_function) {
     char command[SIZE];
     sprintf(command, "nm %s | grep ' %s'", target_process, target_function);
-    
+
     FILE* nm = popen(command, "r");
     if (nm == NULL) {
         printf("Error : Failed to run nm with popen().\n");
@@ -52,7 +53,7 @@ int get_function_offset(char* target_process, char* target_function) {
 // Returns a pointer to the start of the memory section of the target.
 void* get_process_memory(pid_t pid) {
     char filename[SIZE];
-    sprintf(filename, "/proc/%d/maps", pid); 
+    sprintf(filename, "/proc/%d/maps", pid);
 
     FILE* memory_map = fopen(filename, "r");
     if (memory_map == NULL) {
@@ -62,20 +63,20 @@ void* get_process_memory(pid_t pid) {
 
     char buf[SIZE];
     void* address = 0x0;
-    
+
     if (fscanf(memory_map, "%[0-9a-f][^-]", buf) == 0) {
         return NULL;
     }
-    sscanf(buf, "%px", &address); 
+    sscanf(buf, "%px", &address);
     fclose(memory_map);
-    
+
     return address;
 }
 
 int write_in_memory(pid_t pid, long address, unsigned char* buffer, int len) {
     char mem_path[SIZE];
-    sprintf(mem_path, "/proc/%d/mem", pid); 
-    
+    sprintf(mem_path, "/proc/%d/mem", pid);
+
     FILE* process_mem = fopen(mem_path, "wb");
     if (process_mem == NULL) {
         printf("Error : Failed to open target memory.\n");
@@ -91,53 +92,74 @@ int write_in_memory(pid_t pid, long address, unsigned char* buffer, int len) {
 }
 
 int run(int argc, char** argv) {
-    if (argc != 3) {
-        printf("Usage : ./tp [processname] [functionname]\n");
+    if (argc != 4) {
+        printf("Usage : ./tp [processname] [functionname] [function2]\n");
         return -1;
     }
 
     char* ownername = getlogin();
     char* target_process = argv[1];
     char* target_function = argv[2];
+    char* target_function2 = argv[3];
 
     pid_t pid = find_process(ownername, target_process);
     if (pid == 0) {
         printf("Could not obtain process ID. Exiting...\n");
         return -1;
     }
-    printf("Found process ID : %d\n", pid); 
-    
+    printf("Found process ID : %d\n", pid);
+
     ptrace(PTRACE_ATTACH, pid, NULL, NULL);
-    
+
     // We're now attached to the process, so we should be able to read its memory
 
     int offset = get_function_offset(target_process, target_function);
-    if (offset == -1) {
+    int offset2 = get_function_offset(target_process, target_function2);
+    if (offset == -1 || offset2 == -1) {
         printf("Could not obtain target function memory offset. Exiting...\n");
         return -1;
     }
     printf("Target function offset : 0x%x\n", offset);
-    
+
     void* start_address = get_process_memory(pid);
     if (start_address == NULL) {
         printf("Could not determine target process memory address. Eiting...\n");
         return -1;
     }
     printf("Target process memory starts at : %p\n", start_address);
-    
-    unsigned char instr = 0xCC;
 
-    if (write_in_memory(pid, (long) start_address+offset, &instr, 1) == 0) {
+    unsigned char instr_trap = 0xCC;
+
+    if (write_in_memory(pid, (long) start_address+offset, &instr_trap, 1) == 0) {
         printf("Could not write in target process memory. Exiting...\n");
         return -1;
     }
     int status;
-    struct user_regs_struct regs;
+    struct user_regs_struct old_regs, regs, new_regs;
     ptrace(PTRACE_CONT, pid, NULL, NULL);
     while (WSTOPSIG(status) != 5) wait(&status);
     printf("Got a signal : %s\n", strsignal(WSTOPSIG(status)));
+    ptrace(PTRACE_GETREGS, pid, 0, &old_regs);
     ptrace(PTRACE_GETREGS, pid, 0, &regs);
     printf("%llx\n", regs.rip);
+
+    regs.rax = (long)start_address + offset2;
+    regs.rdi = 42; // first argument
+    ptrace(PTRACE_SETREGS, pid, 0, &regs);
+    unsigned char call[2] = {0xFF, 0xD0};
+    write_in_memory(pid, (long)start_address + offset + 1, call, 2);
+    write_in_memory(pid, (long)start_address + offset + 3, &instr_trap, 1);
+
+    int status2;
+    ptrace(PTRACE_CONT, pid, NULL, NULL);
+    while (WSTOPSIG(status2) != 5) wait(&status2);
+    ptrace(PTRACE_GETREGS, pid, 0, &new_regs);
+    printf("Return value: %lld\n", new_regs.rax);
+
+    old_regs.rip = old_regs.rip + 4;
+    ptrace(PTRACE_SETREGS, pid, 0, &old_regs);
+
+
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
 
     return 0;
--- target.c
+++ target.c
@@ -3,12 +3,14 @@
 int target_function2(int i) {
   printf("You have visited the target_function2 function, well done!\n");
   printf("Your argument was %d\n", i);
-  return 0;
+  return 69;
 }
 
 int target_function() {
+    printf("Going in!\n");
     int a;
     scanf("%d", &a);
+    printf("Going out!\n");
     return a;
 }
 
@@ -21,6 +23,7 @@ int main(int argc, char** argv) {
     int var;
     while(1) {
         var = target_function();
+        printf("main %d\n", var);
     }
 
     return 0;
