f9b357de9d49510bd2d89e487f9009ebeec461c7
Skepar <skepar@skepar.org>
2020-12-13 18:05:35 +0100

    Changed the target to use sleep instead of scanf to avoid busy waiting

--- target.c
+++ target.c
@@ -1,20 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int target_function() {
+    printf("Entering original target_function!\n");
+    int a = 5;
+    return a;
+}
+
+// used for a previous challenge
 int target_function2(int i) {
   printf("You have visited the target_function2 function, well done!\n");
   printf("Argument : %d\n", i);
   return 43;
 }
 
-int target_function() {
-    printf("Running original target_function!\n");
-    int a = 5;
-    scanf("%d", &a);
-    printf("Exiting original target_function!\n");
-    return a;
-}
-
+// used for a previous challenge
 void target_function3(int* a) {
     printf("You have visited the target_function3 function, well done!\n");
     printf("Address: %p, value\n", a);
@@ -30,14 +30,14 @@ int optimised() {
 // By default, the program executes target_function
 // User input is asked to avoid busy waiting
 int main(int argc, char** argv) {
-    int var, input;
-    var = 0;
-    printf("Once the code injection is done, enter a number to continue.\n");
+    int var = 0;
+    printf("This program will run target_function every 5 seconds.\n");
+    
     while(1) {
         printf("Running main loop...\n");
-        scanf("%d", &input);
+        sleep(5);
         var = target_function();
-        printf("Return value : %d\n", var);
+        printf("Return value: %d\n", var);
     }
 
     return 0;
